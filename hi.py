320ab43c7c7fdb6a9f3747818a45a4b47015aec8
doryhu <60865153+doryhu@users.noreply.github.com>
2020-02-10 14:17:47 +0800

    1st version
    1st version!!

---
+++ hi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 09:48:51 2020
+
+@author: candice
+"""
+
+print('I\'m Eric \nHe is \"cool\"')
+#print("2: I'm Eric")
+#print('2: He is "cool"')
+name = input("name:")
+print("Hi "+name)
+
